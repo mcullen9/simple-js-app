ecdc318f926abae65a2e1ec053d51d1f0b7ccc1e
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-11-03 18:25:48 -0400

    scripts.js
    correct js file for simple-js-app

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [{name: "Bulbasaur", height: 7, type: ['grass', 'poison']}, {name: "Charizard", height: 10, type: ['fire', 'flying']}, {name: "Jigglypuff", height: 4, type: ['normal', 'fairy']}];
