e98a1c30ad14934a967caa0682f13d8c28ebb6ee
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-11-14 13:01:46 -0500

    Update scripts.js
    new lines for each item in the pokemonList array; added a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-let pokemonList = [{name: "Bulbasaur", height: 7, type: ['grass', 'poison']}, {name: "Charizard", height: 10, type: ['fire', 'flying']}, {name: "Jigglypuff", height: 4, type: ['normal', 'fairy']}];
+let pokemonList = [
+    {name: "Bulbasaur", height: 7, type: ['grass', 'poison']}, 
+    {name: "Charizard", height: 10, type: ['fire', 'flying']}, 
+    {name: "Jigglypuff", height: 4, type: ['normal', 'fairy']}
+];
+
+for (let i = 0; i < pokemonList[i].length; i++){
+    document.write (pokemonList[i].name + ' (height:' + pokemonList[i].height + ')');
+}
