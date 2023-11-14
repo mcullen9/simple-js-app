f0588629d36614e858328bf687b72c7833f595cc
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-11-14 14:23:46 -0500

    Update scripts.js
    added the height to the end of each pokemon's name and a conditional with a threshold of 5

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,12 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList[i].length; i++){
-    document.write (pokemonList[i].name + ' (height:' + pokemonList[i].height + ')');
+  document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + ")");
+ if (pokemonList[i].height > 5){
+    document.write(' - Wow! That\'s a big!');
+ }
 }
+
+ // if (pokemonList[i].height <11){
+  //  document.write (pokemonList[i].name + ' (height:' + pokemonList[i].height + ')');
+   // }
