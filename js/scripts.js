3f93ddf37718caccd3187410a2fff7804d2b1fff
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-11-27 16:09:05 -0500

    Update scripts.js
    overwrite

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,9 @@ let pokemonList = [
     {name: "Charizard", height: 10, type: ['fire', 'flying']}, 
     {name: "Jigglypuff", height: 4, type: ['normal', 'fairy']}
 ]
-/*
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write("<p>" + pokemonList[i].name + " " + "height: " + pokemonList[i].height + ")" + "</p>");
- if (pokemonList[i].height > 5){
-    document.write(' - Wow! That\'s a big!');
- }
-} */
 
-  pokemonList.forEach (function (pokemon) {
-    document.write("<p>" + pokemon.name + " " + "height: " + pokemon.height + ")" + "</p>")
-  })
 })()
+
+pokemonList.forEach (function (pokemon) {
+  document.write("<p>" + pokemon.name + " " + "height: " + pokemon.height + ")" + "</p>")
+})
