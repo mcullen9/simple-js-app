41b27f5e46e8c6d48adce07a9020fbc3dcf8c22a
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-12-20 21:24:48 -0500

    Update scripts.js
    updated syntax changed target item to pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -50,14 +50,14 @@ let pokemonRepository = (function () {
       })
     }
 
-    function loadDetails(item) {
-      let url = item.detailsUrl;
+    function loadDetails(pokemon) {
+      let url = pokemon.detailsUrl;
       return fetch(url).then(function (response) {
         return response.json();
         }).then(function (details) {
-          item.imageUrl = details.sprites.front_default;
-          item.height = details.height;
-          item.types = details.types;
+          pokemon.imageUrl = details.sprites.front_default;
+          pokemon.height = details.height;
+          pokemon.types = details.types;
         }).catch(function (e) {
           console.error(e);
         });
