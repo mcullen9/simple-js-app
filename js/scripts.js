c7d03204b6975f3eb7b514bcf1b8742644195429
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-12-20 20:04:25 -0500

    Update scripts.js
    inserted a modal, using showModal function and hideModal function with event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -61,6 +61,66 @@ let pokemonRepository = (function () {
         });
     }
     
+    function showModal(title, text) {
+      let modalContainer = document.querySelector('#modal-container');
+    
+      // Clear all existing modal content
+      modalContainer.innerHTML = '';
+    
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+    
+      // Add the new modal content
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+    
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+    
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
+    
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+    
+    
+    
+      modalContainer.classList.add('is-visible');
+    }
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+
+  }
+
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  // THE RETURN STATEMENT HERE
+})();
+
+
   return {
     getAll: getAll,
     add: add,
