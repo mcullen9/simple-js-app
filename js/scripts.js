de9409cbab27b6ff6e231867bf3ee7c6f594999e
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2023-12-21 14:02:12 -0500

    Update scripts.js
    rearranging syntax

--- js/scripts.js
+++ js/scripts.js
@@ -99,6 +99,12 @@ let pokemonRepository = (function () {
       modalContainer.classList.remove('is-visible');
     }
 
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
+    });
+
     modalContainer.addEventListener('click', (e) => {
       // Since this is also triggered when clicking INSIDE the modal
       // We only want to close if the user clicks directly on the overlay
@@ -106,8 +112,11 @@ let pokemonRepository = (function () {
       if (target === modalContainer) {
         hideModal();
       }
+    
+      document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal('Modal title', 'This is the modal content!');
+      });
     });
-
   
     return {
       getAll: getAll,
@@ -118,21 +127,9 @@ let pokemonRepository = (function () {
       loadDetails: loadDetails,
       showModal: showModal
     };
+
   })();
 
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();  
-    }
-  });
-
-
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Modal title', 'This is the modal content!');
-  });
-
-  
 
   
 
