27040bb21aed5427dc2659d7eb6cb0a38ee3c070
mcullen9 <145720205+mcullen9@users.noreply.github.com>
2024-01-06 00:21:24 -0500

    Update scripts.js
    applied Bootstrap utility classes to showModal function

--- js/scripts.js
+++ js/scripts.js
@@ -72,69 +72,28 @@ let pokemonRepository = (function () {
     }
     
     function showModal(pokemon) {
-      //Clear all existing modal content
-      modalContainer.innerHTML = '';
-      //Bootstrap
+    
       let modalBody = $(".modal-body");
       let modalTitle = $(".modal-title");
       let modalHeader = $(".modal-header");
-      //Bootstrap clear modal get rid of modalContainer innerHTML line?
+      
       modalTitle.empty();
       modalBody.empty();
+      
+      let nameElement = $("<h1>" + pokemon.name + "</h1>");
+      let heightElement = $("<p>" + "height : " + pokemon.height + "</p>");
+      let myImage = $('<img class="modal-img" style="width:50%">');
+      myImage.attr("src", pokemon.imageUrl);
     
-      let modal = document.createElement('div');
-      modal.classList.add('modal');
-    
-      // Add the new modal content
-      //Delete closeButtonElement bc of Bootstrap?
-     // let closeButtonElement = document.createElement('button');
-     // closeButtonElement.classList.add('modal-close');
-     // closeButtonElement.innerText = 'Close';
-     // closeButtonElement.addEventListener('click', hideModal);
-    
-      let titleElement = document.createElement('h1');
-      titleElement.innerText = pokemon.name;
-    
-      let contentElement = document.createElement('p');
-      contentElement.innerText = 'Height:' + ' ' + pokemon.height;
-
-      let myImage = document.createElement('img');
-      myImage.src = pokemon.imageUrl;
-      modal.appendChild(myImage);
-    
-     // modal.appendChild(closeButtonElement);
-      modal.appendChild(titleElement);
-      modal.appendChild(contentElement);
-      modalContainer.appendChild(modal);
-    
-    
-    
-      modalContainer.classList.add('is-visible');
+      modalTitle.append(nameElement);
+      modalBody.append(myImage); 
+      modalBody.append(heightElement);
+ 
+      $("#modal-container").click(function (){
+        $(button).toggle("modal");
+        });
     }
 
-   // function hideModal() {
-    //  modalContainer.classList.remove('is-visible');
-    //}
-
-    window.addEventListener('keydown', (e) => {
-      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-        hideModal();  
-      }
-    });
-
-    modalContainer.addEventListener('click', (e) => {
-      // Since this is also triggered when clicking INSIDE the modal
-      // We only want to close if the user clicks directly on the overlay
-      let target = e.target;
-      if (target === modalContainer) {
-        hideModal();
-      }
-    
-      document.querySelector('#show-modal').addEventListener('click', () => {
-        showModal('Modal title', 'This is the modal content!');
-      });
-    });
-  
     return {
       getAll: getAll,
       add: add,
@@ -145,7 +104,7 @@ let pokemonRepository = (function () {
       showModal: showModal
     };
 
-  }) ();
+  })();
 
 
   
